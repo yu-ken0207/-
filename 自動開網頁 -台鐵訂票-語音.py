f2a576cdc5e0aa6d6ea2e2a4de583d8a0f598170
yu-ken0207 <72907891+yu-ken0207@users.noreply.github.com>
2022-03-13 15:24:48 +0800

    Update 自動開網頁 -台鐵訂票-語音.py

--- 自動開網頁 -台鐵訂票-語音.py
+++ 自動開網頁 -台鐵訂票-語音.py
@@ -13,28 +13,28 @@ driver.get("http://www.railway.gov.tw/tra-tip-web/tip/tip001/tip121/query")
 
 
 element = driver.find_element_by_name("pid");
-element.send_keys("u122093329");#輸入內容
+element.send_keys("身分證號");#輸入內容
 
 
 
 
 element = driver.find_element_by_name("startStation");
-element.send_keys("7000-花蓮");#輸入起點
+element.send_keys("地點");#輸入起點
 
 
 
 element = driver.find_element_by_name("endStation");
-element.send_keys("3300-臺中");#輸入終點
+element.send_keys("地點");#輸入終點
 
 
 
 driver.find_element_by_id("rideDate1").clear();
 element = driver.find_element_by_id("rideDate1");
-element.send_keys("20200229");#輸入日期
+element.send_keys("YYYMMDD");#輸入日期
 
 
 element = driver.find_element_by_name("ticketOrderParamList[0].trainNoList[0]");
-element.send_keys("283");#輸入車次
+element.send_keys("###");#輸入車次
 
 time.sleep(1)
 click1=driver.find_element_by_id("switchToVoice")#切換換語音
